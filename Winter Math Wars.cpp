07b4663794057560158846e47fbad81452def51a
unknown <rishikrishnan87@gmail.com>
2024-08-06 09:44:32 -0400

    Adding Background

--- Winter Math Wars.cpp
+++ Winter Math Wars.cpp
@@ -1,20 +1,69 @@
-// Winter Math Wars.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// include important libraries here
 
+#include <sstream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <fstream>
+
+// Make code easier to type by using "using namespace"
+using namespace sf;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Creating a video mode object
+    VideoMode vm(960, 540);
+
+    //Creating and opening a window for the game
+    RenderWindow window(vm, "Winter Math Wars..!!!");
+    View view(sf::FloatRect(0, 0, 1920, 1080));
+    window.setView(view);
+
+    /*
+    * Background
+    */
+
+    //Create a texture to hold a graphic on the GPU
+    Texture textureBackground;
+
+    //Load a graphic into the texture
+    textureBackground.loadFromFile("graphics/background.png");
+
+    //Creating a sprite for background;
+    Sprite spriteBackground;
+
+    // Attaching background sprite to its texture
+    spriteBackground.setTexture(textureBackground);
+
+    // Set the sprite background to cover the screen
+    spriteBackground.setPosition(0, 0);
+
+    // Main Game Loop
+    while (window.isOpen()) {
+        /*
+        * *****Handle the players input*****
+        */
+
+
+        /*
+        * *****Update the scene*****
+        */
+
+        /*
+        * *****Draw the scene*****
+        */
+
+        //Clear everything from the last frame
+        window.clear();
+
+        //Draw our game scene here
+        window.draw(spriteBackground);
+
+        // Show everything we just drew
+        window.display();
+
+
+    }
+    return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
