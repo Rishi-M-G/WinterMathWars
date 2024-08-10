ff072653e7a80844197e93452c1b68cd103fa249
unknown <rishikrishnan87@gmail.com>
2024-08-10 06:38:23 -0400

    Testing: Zombie, Scale and Location,Flipping sprite

--- Winter Math Wars.cpp
+++ Winter Math Wars.cpp
@@ -38,11 +38,45 @@ int main()
     // Set the sprite background to cover the screen
     spriteBackground.setPosition(0, 0);
 
+    /*
+    * Player Test
+    */
+
+    //Create a texture to hold a graphic on the GPU
+    Texture textureZombie1;
+
+    //Load the zombie graphic into the texture
+    textureZombie1.loadFromFile("graphics/Zombie_Villager.png");
+
+    //Create a sprite for the zombie
+    Sprite spriteZombie1;
+
+    //Attach the zombie sprite to the tree texture
+    spriteZombie1.setTexture(textureZombie1);
+
+
+    // Set the scale of the zombie sprite to scale it down
+    float scaleX = 0.35f; 
+    float scaleY = 0.35f; 
+    spriteZombie1.setScale(scaleX, scaleY);
+
+    // Set the scale to flip the zombie sprite horizontally
+    //spriteZombie1.scale(-1.0f, 1.0f);
+
+    //Set the position for the tree sprite
+    spriteZombie1.setPosition(500, 300);
+
     // Main Game Loop
     while (window.isOpen()) {
         /*
         * *****Handle the players input*****
         */
+
+        // If the player presses Escape - Window Close
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
 
 
         /*
@@ -58,6 +92,9 @@ int main()
 
         //Draw our game scene here
         window.draw(spriteBackground);
+
+        // Draw the Zombie
+        window.draw(spriteZombie1);
 
         // Show everything we just drew
         window.display();
