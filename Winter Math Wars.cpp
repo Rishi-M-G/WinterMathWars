89ddea55ac24d312e682c4de171f126698f2c448
unknown <rishikrishnan87@gmail.com>
2024-08-20 16:15:26 -0400

    Backlog changes

--- Winter Math Wars.cpp
+++ Winter Math Wars.cpp
@@ -9,6 +9,20 @@
 // Make code easier to type by using "using namespace"
 using namespace sf;
 
+// Snowball function declaration
+void updateSnowballs(int seed); // Function Prototype
+
+const int NUM_SNOWBALLS = 4;
+
+Sprite snowballs[NUM_SNOWBALLS]; 
+
+// Where is the Snowball?
+// TOP, MIDDLE, BOTTOM or NONE
+
+enum class side {TOP,MIDDLE,BOTTOM,NONE};
+side snowballPositions[NUM_SNOWBALLS]; // we will have an array called snowballPositions with four values in it. Each of these values is of the side type and can be either TOP, MIDDLE, BOTTOM, or NONE.
+
+
 int main()
 {
     //Creating a video mode object
@@ -39,32 +53,109 @@ int main()
     spriteBackground.setPosition(0, 0);
 
     /*
-    * Player Test
+    * Player
     */
 
     //Create a texture to hold a graphic on the GPU
-    Texture textureZombie1;
+    Texture texturePlayer;
 
     //Load the zombie graphic into the texture
-    textureZombie1.loadFromFile("graphics/Zombie_Villager.png");
+    texturePlayer.loadFromFile("graphics/Zombie_Villager.png");
 
     //Create a sprite for the zombie
-    Sprite spriteZombie1;
+    Sprite spritePlayer;
 
     //Attach the zombie sprite to the tree texture
-    spriteZombie1.setTexture(textureZombie1);
+    spritePlayer.setTexture(texturePlayer);
 
 
     // Set the scale of the zombie sprite to scale it down
-    float scaleX = 0.35f; 
-    float scaleY = 0.35f; 
-    spriteZombie1.setScale(scaleX, scaleY);
+    float scalePlayerX = 0.35f; 
+    float scalePlayerY = 0.35f; 
+    spritePlayer.setScale(scalePlayerX, scalePlayerX);
 
     // Set the scale to flip the zombie sprite horizontally
     //spriteZombie1.scale(-1.0f, 1.0f);
 
     //Set the position for the tree sprite
-    spriteZombie1.setPosition(500, 300);
+    spritePlayer.setPosition(100, 300);
+
+    /*
+   * Idle Snowman
+   */
+   
+    Texture textureSnowman;
+
+    //Load the zombie graphic into the texture
+    textureSnowman.loadFromFile("graphics/snowman.png");
+
+    // Create a Sprite for NPC
+    Sprite spriteSnowman[2];
+
+    for (int i = 0; i < 2; i++) {
+        spriteSnowman[i].setTexture(textureSnowman);
+    }
+
+    // Set the scale of the NPC sprite to scale it down
+    float scaleSnowmanX = 0.65f;
+    float scaleSnowmanY = 0.65f;
+
+    for (int i = 0; i < 2; i++) {
+        spriteSnowman[i].setScale(scaleSnowmanX, scaleSnowmanY);
+        //spriteSnowman[i].scale(-1.0f, 1.0f);
+    }
+
+    //Set the position for the tree sprite
+    spriteSnowman[0].setPosition(100, 100);
+    spriteSnowman[1].setPosition(100, 500);
+
+
+    /*
+    * NPC
+    */
+
+    // NPCs
+    Texture textureNPC;
+
+    //Load the zombie graphic into the texture
+    textureNPC.loadFromFile("graphics/NPC.png");
+
+    // Create a Sprite for NPC
+    Sprite spriteNPCs[3];
+
+    for (int i = 0; i < 3; i++) {
+        spriteNPCs[i].setTexture(textureNPC);
+    }
+
+    // Set the scale of the NPC sprite to scale it down
+    float scaleNPCX = 0.35f;
+    float scaleNPCY = 0.35f;
+
+    for (int i = 0; i < 3; i++) {
+        spriteNPCs[i].setScale(scaleNPCX, scaleNPCY);
+        spriteNPCs[i].scale(-1.0f, 1.0f);
+    }
+
+    //Set the position for the tree sprite
+    spriteNPCs[0].setPosition(1800, 100);
+    spriteNPCs[1].setPosition(1800, 300);
+    spriteNPCs[2].setPosition(1800, 500);
+
+    /*
+    * Preparing Snowballs
+    */
+
+    Texture textureSnowball;
+
+    textureSnowball.loadFromFile("graphics/snowball.png");
+
+    // Setting up textures for each snowball
+    for (int i = 0; i < NUM_SNOWBALLS; i++)
+    {
+        snowballs[i].setTexture(textureSnowball);
+        snowballs[i].setPosition(-2000, 2000);
+    }
+
 
     // Main Game Loop
     while (window.isOpen()) {
@@ -93,8 +184,18 @@ int main()
         //Draw our game scene here
         window.draw(spriteBackground);
 
-        // Draw the Zombie
-        window.draw(spriteZombie1);
+        // Draw the Player
+        window.draw(spritePlayer);
+
+        // Draw the Snowman
+        for (int i = 0; i < 2; i++) {
+            window.draw(spriteSnowman[i]);
+        }
+
+        // Draw the NPCs
+        for (int i = 0; i < 3; i++) {
+            window.draw(spriteNPCs[i]);
+        }
 
         // Show everything we just drew
         window.display();
